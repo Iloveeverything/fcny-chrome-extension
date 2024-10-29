b8564346b6b5ec8414234b95aaf81ec2458dc5ad
Kiki Hunt <Katharinelohunt@gmail.com>
2024-10-29 10:30:52 -0400

    day1

--- manifest.js
+++ manifest.js
@@ -1,10 +1,11 @@
+
 {
-    "name": "Hello Extensions",
-    "description": "Base Level Extension",
-    "version": "1.0",
-    "manifest_version": 3,
-    "action": {
-      "default_popup": "hello.html",
-      "default_icon": "hello_extensions.png"
-    }
+  "name": "Metronome Extension",
+  "description": "Music Tool Extensions",
+  "version": "1.0",
+  "manifest_version": 3,
+  "action": {
+    "default_popup": "hello.html",
+    "default_icon": "hello_extensions.png"
   }
+}
--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-console.log('This is a popup!');
+//Find  start button element by ID and store reference to in variable named startButton.
+//Find tempo slider element by ID and store reference to in variable named tempoSlider
+// Find bpm display element by ID and store reference to in variable named bpmDisplay
+// Assign the value of tempoSlider to innerHTML property bpmDisplay.
+//
+//oninput event occurs when the value of an <input> element is changed.
+
+
+
+
+
+
+
